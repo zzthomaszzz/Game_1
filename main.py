4b13db7b8fe282c6f7e8aadc5d24b67728e5ae5d
Thomas Minh Nguyen <thomasnguyen8503@gmail.com>
2023-06-05 21:23:33 +1200

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+import sys
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(60)
